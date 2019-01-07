e29098e749672e6ad1a162665c5ddd9dbda16e3f
FedeBa <43404849+FedeBa@users.noreply.github.com>
2019-01-07 16:20:23 +0100

    Update string_square.js

--- string_square.js
+++ string_square.js
@@ -1,9 +1,9 @@
 function string_square(s){
-	if (typeof s == 'string'){
-	return (s.length*s.length);
-	}
-	else {return -1;}
+    if (typeof(s)==='string'){
+        return s.length*s.length;
+    }
+    return -1;
 }
 
-module.exports = string_square;
+module.exports = {string_square}
 
